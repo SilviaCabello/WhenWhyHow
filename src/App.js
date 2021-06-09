744317f5a4d1dec63fc64462911e056185373bd9
Francesco <france.luciani@gmail.com>
2021-06-09 14:59:10 +0200

    add spending files

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home/Home";
 import LogIn from "./components/LogIn/LogIn";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Balances from "./components/Balances/Balances";
+import Spending from "./components/Spending/Spending";
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/balances">
           <Balances />
+        </Route>
+        <Route path="/spending">
+          <Spending />
         </Route>
         <Route exact path="/">
           <Home />
---
+++ src/components/Spending/Spending.js
@@ -0,0 +1,7 @@
+import "./Spending.css";
+
+function Spending() {
+  return <div className="container">this is a Spending component</div>;
+}
+
+export default Spending;
