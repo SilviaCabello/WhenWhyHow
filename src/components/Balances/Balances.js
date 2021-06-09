0fffd808ab76557811d9a973ce6626bfd4ce9a6a
SilviaCabello <silviacabellom@gmail.com>
2021-06-09 11:45:48 +0200

    add categories and link spendings

--- src/components/Balances/Balances.js
+++ src/components/Balances/Balances.js
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
 import "./Balances.css";
 
 function Balances() {
   return (
-    <div className="container">
-      <p>This is balances component</p>
+    <div>
+      <p className="balances-totalcash">Available Cash 5388,23€</p>
+      <div className="balances-categories">
+        <Link to={`/dashboard`}>
+          <img className="spendings-category" src="https://i.imgur.com/9Z8HrXN.png" alt="Spendings" />
+        </Link>
+        <img src="https://i.imgur.com/8CspobY.png" alt="Banking Habits" />
+        <img src="https://i.imgur.com/uE4yByP.png" alt="Digital chanels" />
+      </div>
     </div>
   );
 }
