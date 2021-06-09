7e6ebb7780601e8765bd579239ab8298e731589e
SilviaCabello <silviacabellom@gmail.com>
2021-06-09 20:24:28 +0200

    change from dropdown to collapse accordion

--- src/components/Balances/MainBank.js
+++ src/components/Balances/MainBank.js
@@ -1,37 +1,64 @@
 import React, { useState } from "react";
 import "./Balances.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-} from "reactstrap";
+import { Accordion, Card, Button } from "react-bootstrap";
 
-function MainBank() {
-  const [dropdown, setDropdown] = useState(false);
-  const openCloseDropdown = () => {
-    setDropdown(!dropdown);
-  };
-
+export default function MainBank() {
   return (
     <div>
-      <Dropdown isOpen={dropdown} toggle={openCloseDropdown} size="lg">
-        <DropdownToggle className="dropdown-MainBank" caret>
-          Main Bank
-        </DropdownToggle>
-
-        <DropdownMenu className="Records" right>
-          <DropdownItem header>These are your last Records:</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Record Example Number 1</DropdownItem>
-          <DropdownItem>Record Example Number 2</DropdownItem>
-          <DropdownItem>Record Example Number 3</DropdownItem>
-          <DropdownItem>Record Example Number 4</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
+      <Accordion>
+        <Card>
+          <Card.Header className="card-header-mainbank">
+            <Accordion.Toggle
+              className="toggle-MainBank"
+              as={Button}
+              variant="link"
+              eventKey="0"
+            >
+              Main Bank ▼
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <ul className="MainBank-Records">
+                <li>Record example number 1</li>
+                <li>Record example number 2</li>
+                <li>Record example number 3</li>
+              </ul>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header className="card-header-secondbank">
+            <Accordion.Toggle
+              className="toggle-SecondBank"
+              as={Button}
+              variant="link"
+              eventKey="0"
+            >
+              Second Bank ▼
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Records</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header className="card-second-mainbank">
+            <Accordion.Toggle
+              className="toggle-SecondBank"
+              as={Button}
+              variant="link"
+              eventKey="0"
+            >
+              Third Bank ▼
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Records</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     </div>
   );
 }
-
-export default MainBank;
