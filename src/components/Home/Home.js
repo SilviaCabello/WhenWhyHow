fbd75065fb95334329b78f70966ca58d6d28fab2
HELENTRISHA <helen-trisha_student2021@wilder.school>
2021-06-11 09:26:40 +0200

    starting homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,14 @@ import "./Home.css";
 function Home() {
   return (
     <div className="container">
-      <p>This is home component</p>
+      <img
+        className="picture"
+        src="https://res.cloudinary.com/dnefeccae/image/upload/v1623246641/pexels-andrea-piacquadio-3781524_y1sly8.jpg"
+        alt=""
+      />
+      <div className="explore-profile">
+        <button className="profile-btn">Explore your Profile</button>
+      </div>
     </div>
   );
 }
