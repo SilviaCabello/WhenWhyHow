a4c68e8dfda3c0cd1667496badbeb72db95c389c
HELENTRISHA <helen-trisha_student2021@wilder.school>
2021-06-15 16:49:52 +0200

    login component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
@@ -12,12 +13,39 @@ import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
 
 library.add(fab, faCheckSquare);
 function App() {
+  const adminUser = {
+    user: "helen",
+    password: "admin123",
+  };
+  const [user, setUser] = useState({ name: "", user: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+    if (
+      details.user == adminUser.user &&
+      details.password == adminUser.password
+    ) {
+      console.log("logged in");
+      setUser({
+        user: details.user,
+        password: details.password,
+      });
+    } else {
+      console.log("details do not match");
+      setError("details do not match");
+    }
+  };
+
+  const Logout = () => {
+    setUser({});
+  };
   return (
     <div className="App">
       <NavBar />
       <Switch>
         <Route path="/login">
-          <LogIn />
+          <LogIn Login={Login} error={error} />
         </Route>
         <Route path="/dashboard">
           <Dashboard />
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,26 +1,54 @@
 import "./LogIn.css";
+import { useState } from "react";
 
-function LogIn() {
+function LogIn({ Login, error }) {
+  const [details, setDetails] = useState({ user: "", password: "" });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    Login(details);
+  };
   return (
-    <div className="container">
-      <div className="signin-container">
-        <div className="title-signin">
-          <p>Sign in to Enter Your Profile</p>
+    <form onSubmit={submitHandler}>
+      <div className="login-container">
+        <div className="signin-container">
+          <div className="title-signin">
+            <p>Sign in to Enter Your Profile</p>
+          </div>
         </div>
-      </div>
-      <div className="label-username">
-        <label>Username</label>
+
+        <div className="label-user">
+          <label htmlFor="user">User:</label>
+          <input
+            type="text"
+            username="user"
+            id="user"
+            onChange={(e) => setDetails({ ...details, user: e.target.value })}
+            value={details.user}
+          />
+        </div>
         <div className="label-password">
-          <label>Password</label>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            password="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
         </div>
         <div className="forgotten-psw">
           <p>Forgot your password?</p>
         </div>
+
         <div className="signin-btn">
-          <button>Sign In </button>
+          <input type="submit" value="LOGIN" />
+          {error != "" ? <div className="error">{error}</div> : ""}
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 
