2f052f892b3b452bf1ec51860d68235629efc71b
Silvia Cabello <78726341+SilviaCabello@users.noreply.github.com>
2021-07-12 14:04:40 +0200

    Delete backend directory

--- backend/conf.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql2");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "wilhelmine24",
-  database: "registration",
-});
-module.exports = connection;
